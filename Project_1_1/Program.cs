a212a75200027ef2471ad9181f94580c460eb2ce
stefankarlsson1230@gmail.com <stefankarlsson1230@gmail.com>
2025-08-07 10:18:01 +0200

    Finished Project_1_1

---
+++ Project_1_1/Program.cs
@@ -0,0 +1,5 @@
+﻿WriteLine("What is C#? - It's a general-purpose programming language.");
+WriteLine("Why learn to code? - To be able to create games in Unity, create webapps with Blazor, and perhaps to get a job.");
+WriteLine("I’m learning C#!");
+WriteLine("C# can, for example, build games, web applications and desktop programs.");
+WriteLine("A program solves a problem. It does this by describing the solution in a programming language, which is then compiled into an executable program");
