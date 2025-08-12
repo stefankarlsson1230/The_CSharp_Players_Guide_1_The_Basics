04d1708f50c7c0fbb2bbddec83c15c599df7b03f
stefankarlsson1230@gmail.com <stefankarlsson1230@gmail.com>
2025-08-12 10:31:44 +0200

    Finished Project_4_1

---
+++ Project_4_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine();  // The thing we are talking about
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine();  // Our description of the thing
+string c = "Doom";   /*  c stores the string "of Doom". I also removed an unneccesary "of" */
+string d = "3000";      /*  d stores the string "3000" */
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
+
+
+// Answer this question: Aside from comments, what else could you do to make this code more understandable?
+// More descriptive variable names
