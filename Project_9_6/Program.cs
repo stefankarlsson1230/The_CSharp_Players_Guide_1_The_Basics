d3aa84ea655739dd2c35cc4accc1f281b2b50934
stefankarlsson1230@gmail.com <stefankarlsson1230@gmail.com>
2025-08-17 19:41:36 +0200

    Finished Project_9_6

---
+++ Project_9_6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Password Gate - Prompt and validate secret password.
+
+Console.WriteLine("(The secret password is \"secret\")");
+Console.Write("Password: ");
+string password = Console.ReadLine();
+
+if (password == "secret")
+{
+    Console.WriteLine("Correct! You may pass");
+}
+else
+{
+    Console.WriteLine("That is NOT the password! Die rebel scum!");
+}
+
