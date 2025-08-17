86e283d0f4a33601ef8fdbb9662f41dbae0fafdf
stefankarlsson1230@gmail.com <stefankarlsson1230@gmail.com>
2025-08-17 19:10:50 +0200

    Finished Project_9_2

---
+++ Project_9_2/Program.cs
@@ -0,0 +1,62 @@
+﻿
+Console.Write("Enter value for x: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Enter value for y: ");
+int y = int.Parse(Console.ReadLine());
+
+string direction = "";
+
+if (x == 0 && y == 0)
+{
+    Console.WriteLine("The Enemy is here!");
+}
+else if (x < 0)
+{
+    if (y < 0)
+    {
+        direction = "southwest";
+    }
+    else if (y == 0)
+    {
+        direction = "west";
+    }
+    else
+    {
+        direction = "northwest";
+    }
+}
+else if (x == 0)
+{
+    if (y < 0)
+    {
+        direction = "south";
+    }
+    else
+    {
+        direction = "north";
+    }
+}
+else if(x > 0)
+{
+    if (y < 0)
+    {
+        direction = "southeast";
+    }
+    else if (y == 0)
+    {
+        direction = "east";
+    }
+    else
+    {
+        direction = "norteast";
+    }
+}
+
+
+if (x != 0 || y != 0)
+{
+    Console.WriteLine($"The enemy is to the {direction}!");
+}
+
+
